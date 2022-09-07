6e658c335329ca96068c5f4ec30043f8c8629bc6
Taysmar <taysmar@outlook.com>
2022-09-07 13:58:58 -0300

    Finalização da estilização da pagina

--- script.js
+++ script.js
@@ -33,7 +33,6 @@ const fetchData = () => {
 
    Promise.all(pokemonPromises)
    .then(pokemons => {
-    //console.log(pokemons)
 
     const lisPokemons = renderPokemons(pokemons)
     
@@ -43,43 +42,3 @@ const fetchData = () => {
 }
 
 fetchData() 
-
-
-
-
-
-
-
-
-
-
-// //Faz a requisição para a API. Na variável pokemon vão estar guardados os dados da resposta em formato json. Retorna os dados do pokemon
-// function fetchPokemon(url) {
-//     const pokemon = fetch(url).then(response => response.json())
-//     return pokemon
-// }
-
-// //Pega as 151 primeiras Urls 
-// async function fetchAllPokemons() {
-//     let allPokemons = []
-//     for (let i = 1; i <= 151; i++){
-//         const url = getPokemonUrl(i)
-//         const pokemonData = await fetchPokemon(url).then(pokemon => allPokemons.push(pokemon))
-//     }
-//     return Promise.all(allPokemons)
-// }
-
-// //EventListener que dispara uma função ao carregar a pagina.
-// window.addEventListener("load", async(event) => {
-//     const allPokemons = await fetchAllPokemons()
-//     console.log(allPokemons)
-//     console.log("terminou")
-// })
-// console.log("test")
-/**
- * Passo a passo do desafio
- * passo 1- pegar os dados dos primeiros pokemons 
- * passo 1.1 - montar as urls de cada pokemon ✔
- * passo 1.2 - fazer a requisição de cada url
- * passo 2 - Exibir os nomes dos pokemons
- */
